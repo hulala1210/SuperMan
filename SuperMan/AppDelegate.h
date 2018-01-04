a2cee244622ed1f86ddbd8c639e2f4b3452861a0
Kevin_Chen <408854137@qq.com>
2018-01-04 20:55:07 +0800

    Initial Commit

---
+++ SuperMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperMan
+//
+//  Created by Kevin Chen on 2018/1/4.
+//  Copyright © 2018年 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
